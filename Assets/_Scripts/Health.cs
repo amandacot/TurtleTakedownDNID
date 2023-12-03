b5ef45d45bc936bcd9888fea7c212b071fa545b9
Maxiss02 <davidhalasz780@gmail.com>
2023-12-03 16:01:53 -0500

    attacking and health fully implemented

--- Assets/_Scripts/Health.cs
+++ Assets/_Scripts/Health.cs
@@ -6,6 +6,8 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private int health = 5;
 
+    private int MAX_HEALTH = 5;
+
     void Update()
     {
         
@@ -13,6 +15,39 @@ public class Health : MonoBehaviour
 
     public void Damage(int amount)
     {
+        if(amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Can't have negative damage");
+        }
         this.health -= amount;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Can't have negative healing");
+        }
+
+        if(health + amount > MAX_HEALTH)
+        {
+            this.health = MAX_HEALTH;
+        }
+        else
+        {
+            this.health += amount;
+        }
+        
+    }
+
+private void Die()
+    {
+        Debug.Log("I am Dead!");
+        Destroy(gameObject);
     }
 }
