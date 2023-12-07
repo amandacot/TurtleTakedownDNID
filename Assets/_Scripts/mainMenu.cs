ce4bc0a1e5d33ad873b9627945110336eda5ca58
Maxiss02 <davidhalasz780@gmail.com>
2023-12-06 20:26:27 -0500

    menu scenes

--- Assets/_Scripts/mainMenu.cs
+++ Assets/_Scripts/mainMenu.cs
@@ -1,18 +1,38 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public string SceneName;
+    Renderer ren;
+
     void Start()
     {
-        
+      
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            
+            Debug.Log("z pressed");
+            StartCoroutine(ChangeScene());
+            SceneManager.LoadScene("cutscene1", LoadSceneMode.Single);
+        }
+    }
+
+    IEnumerator ChangeScene()
+    {
         
+        
+        
+
+        yield return new WaitForSeconds(2f);
+
+
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
     }
 }
